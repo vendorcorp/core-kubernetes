84b56cab081a82df28d29664411e3bfe14eabe2b
Paul Horton <phorton@sonatype.com>
2022-03-17 10:49:51 +0000

    initial port from `iaas-platform`: - Internal DNS - Common kubernetes namespaces
    Signed-off-by: Paul Horton <phorton@sonatype.com>

---
+++ dns.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_zone" "vendorcorp_internal" {
+  name    = "vendorcorp.internal"
+  comment = "Vendor Corp internal DNS Zone"
+  tags    = var.default_resource_tags
+
+  vpc {
+    vpc_id = module.shared_infrastructure.vpc_id
+  }
+}
---
+++ namespaces.tf
@@ -0,0 +1,13 @@
+resource "kubernetes_namespace" "namespace_shared_core" {
+  metadata {
+    # annotations = {
+    #   name = "example-annotation"
+    # }
+
+    # labels = {
+    #   mylabel = "label-value"
+    # }
+
+    name = "shared-core"
+  }
+}
